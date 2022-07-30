d7d4271c3b0c1f16a990effdb02966d0dfde5496
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-07-30 21:38:12 +0530

    added some prints but will still need debugging

--- machine_learning/utils/math.py
+++ machine_learning/utils/math.py
@@ -7,10 +7,20 @@ def get_regression_parameters(X,y):
     return np.matmul(xtx_inverse,xty)
 
 def get_sum_given_feature(data,all_features,xaxis_metric,yxis_metric):
+    print('in find sum function')
+    print('headers received are: ')
+    print(all_features)
+    print('xaxis feature requested is')
+    print(xaxis_metric)
+    print('y axis feature requested is')
+    print(yxis_metric )
     xaxis_index = all_features.index(xaxis_metric)
     yaxis_index = all_features.index(yxis_metric)
+    print(f'index of  {xaxis_metric} is {xaxis_index}')
+    print(f'index of  {yxis_metric} is {yaxis_index}')
     xaxis_values = data[xaxis_index]
     yaxis_values = data[yaxis_index]
+    print(f'value of {xaxis_metric}')
     dict_of_sums_temp = {}
     for index,x in enumerate(xaxis_values):
         if str(int(x)) in dict_of_sums_temp.keys():
