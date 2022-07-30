6f748af60a7216f15116130f6799bd8e01092e31
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-07-30 16:58:50 +0530

    refactored data preparer to return headers along with the data itself

--- machine_learning/logistic_regression/data_loader.py
+++ machine_learning/logistic_regression/data_loader.py
@@ -8,11 +8,11 @@ def get_features_labels(filename):
     data = data_loader.get_data(filename)
 
     data[0]=[i.replace('\ufeff','') for i in data[0]]
-
+    headers = data[0]
     X,y = data_loader.splitdata_to_features_labels(data[1:])
     X = np.array(X).astype(np.float)
     y = np.array(y).astype(np.float)
-    return X,y
+    return headers,X,y
 
 
 def split_data_to_traintest(X,y,test_size=0.3,shuffle=True):
--- machine_learning/logistic_regression/runner.py
+++ machine_learning/logistic_regression/runner.py
@@ -26,10 +26,10 @@ number_of_simulations = 75
 test_set_proportion = 0.4
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, relative_path_to_file)
-X,y = data_loader.get_features_labels(filename)
+headers,X,y = data_loader.get_features_labels(filename)
 print(X.shape)
 print(y.shape)
-
+print(headers)
 
 training_f1_score = []
 testing_f1_score = []
