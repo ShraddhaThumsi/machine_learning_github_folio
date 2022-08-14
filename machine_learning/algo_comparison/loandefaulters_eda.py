ad009b40da6e7807887986d4ea310be52cb0bb65
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-08-14 08:30:43 +0530

    changing course from cecl to classifier comparison. CECL needs a time series data which needs to be searched for better.

--- machine_learning/algo_comparison/loandefaulters_eda.py
+++ machine_learning/algo_comparison/loandefaulters_eda.py
@@ -1,5 +1,6 @@
 relative_path_to_data = '../data/preprocessed_files/kaggle/loan_sub.csv'
 #the loan_sub file has been left out of git because its size exceeds 100mb.
+# I have selected the first 20% of the dataset into my subset to allow for speedy processing.
 # In case you would like to run the code on the same data as I, please reach out to me at
 #thumsishraddhasatish@gmail.com, I will be happy to share the file with you.
 # if you would like to download the original data from Kaggle, it can be found at https://www.kaggle.com/datasets/adarshsng/lending-club-loan-data-csv
@@ -13,6 +14,6 @@ filename = os.path.join(dirname, relative_path_to_data)
 data = data_loader.get_data(filename)
 print(len(data))
 df = pd.read_csv(filename)
-colnames_for_rollingdelinq = ['num_tl_30dpd','num_tl_90g_dpd_24m','num_tl_120dpd_2m']
+colnames_for_rollingdelinq = ['last_credit_pull_d','next_pymnt_d','issue_d','dti','num_sats','num_tl_30dpd','num_tl_90g_dpd_24m','num_tl_120dpd_2m']
 for c in colnames_for_rollingdelinq:
     print(c in data[0])
