e5f790f449c53e989a2dba4c8b452d1a4b6010fa
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-11-27 14:16:59 +0530

    have the working version for removing dups, trying alternate code

---
+++ fundamentals/algorithms/linked_list/remove_dups.py
@@ -0,0 +1,33 @@
+#removing duplicate values from a linked list
+import fundamentals.data_structures.linked_list as linked_list
+list_items=[1,3,4,6,4]
+#the final linked list should be 1->3->4->6->
+head=linked_list.build_ll_from_arr(list_items)
+
+def remove_dups(h):
+    visited={}
+
+    n=h
+    while n is not None:
+        visited[n.data]=False
+        n=n.next
+    n=h
+    """visited[n.data] = True
+    while n is not None:
+        if visited[n.data]:
+            n_prior= n
+            n = n.next
+            if visited[n.data]:
+                n_prior.next = n.next"""
+    while n.next is not None:
+        if visited[n.next.data]:
+            n.next=n.next.next
+        else:
+            visited[n.next.data]=True
+            n=n.next
+    return h
+print('list before removing dups')
+print(linked_list.check_list(head))
+new_head=remove_dups(head)
+print('linked list after removing dups')
+print(linked_list.check_list(new_head))
