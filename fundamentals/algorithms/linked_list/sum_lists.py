d84b4fea00aafb2cd49e26ff3e904bd878900ec5
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-11-28 13:45:42 +0530

    added code for sum of list in forward order

--- fundamentals/algorithms/linked_list/sum_lists.py
+++ fundamentals/algorithms/linked_list/sum_lists.py
@@ -10,7 +10,7 @@ head1=linked_list.build_ll_from_arr(list1)
 head2=linked_list.build_ll_from_arr(list2)
 print(linked_list.check_list(head1))
 print(linked_list.check_list(head2))
-def find_sum_of_list_items(h1, h2):
+def find_sum_of_list_items_in_reverse(h1, h2):
 
     n1=h1
     n2=h2
@@ -23,13 +23,40 @@ def find_sum_of_list_items(h1, h2):
     num1=makenum_from_list(n1,0,0)
     num2=makenum_from_list(n2,0,0)
     return num1+num2
-def listify(sum):
-    digits_of_sum=[]
-    while sum>0:
-        digit=sum%10
-        digits_of_sum.append(digit)
-        sum=sum//10
-    return linked_list.build_ll_from_arr(digits_of_sum)
+def find_sum_of_list_items_forward(h1,h2):
 
-print(find_sum_of_list_items(head1, head2))
-print(linked_list.check_list(listify(find_sum_of_list_items(head1, head2))))
+    size1=linked_list.sizeof_list(h1)
+    size2=linked_list.sizeof_list(h2)
+    def makenum_from_list(temphead,num,pow_to_use):
+        while temphead is not None:
+            num+=temphead.data*(10**pow_to_use)
+            pow_to_use-=1
+            temphead=temphead.next
+        return num
+    num1=makenum_from_list(h1,0,size1-1)
+    num2=makenum_from_list(h2,0,size2-1)
+    return num1+num2
+def listify(sum,reversed_flag=True):
+    digits_of_sum=[]
+    while sum > 0:
+        digit = sum % 10
+        digits_of_sum.append(digit)
+        sum = sum // 10
+    if reversed_flag:
+
+        return linked_list.build_ll_from_arr(digits_of_sum)
+    else:
+        return linked_list.build_ll_from_arr(list(reversed(digits_of_sum)))
+
+print(find_sum_of_list_items_in_reverse(head1, head2))
+print(linked_list.check_list(listify(find_sum_of_list_items_in_reverse(head1, head2),reversed_flag=True)))
+
+
+list1 = [6,1,7] #7->1->6=617
+list2 = [2,9,5] #5->9->2=295 .... Therefore the list returned should be the value 912, i.e. 2->1->9
+head1=linked_list.build_ll_from_arr(list1)
+head2=linked_list.build_ll_from_arr(list2)
+print(linked_list.check_list(head1))
+print(linked_list.check_list(head2))
+print(find_sum_of_list_items_forward(head1,head2))
+print(linked_list.check_list(listify(find_sum_of_list_items_forward(head1,head2),reversed_flag=False)))
