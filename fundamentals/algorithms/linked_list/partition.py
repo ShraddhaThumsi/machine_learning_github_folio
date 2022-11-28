d251144943f85fa8f6dd5788f592e517d9f72ce2
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-11-28 12:31:14 +0530

    partitioned list code, more test cases

--- fundamentals/algorithms/linked_list/partition.py
+++ fundamentals/algorithms/linked_list/partition.py
@@ -2,7 +2,7 @@ import fundamentals.data_structures.linked_list as linked_list
 #given a node in a linked list, split the list elements such that all smaller values are to the left and equal and greater values to the right.
 # Then return the long list with the elements all stitched together
 
-list_items=[1,2,3,4,5,3,4,5,6,7,6,7,8,9,10]
+list_items=[10,1,1,2,2,3,19,8,7,18,34,5,6,3,2,6,4,7,9]
 head=linked_list.build_ll_from_arr(list_items)
 print(linked_list.check_list(head))
 def partition_list_by_node(head,node):
@@ -10,7 +10,7 @@ def partition_list_by_node(head,node):
     lt_items=[]
     gt_eq_items=[]
     while n is not None:
-        if n.data < node.data:
+        if int(n.data) < int(node.data):
             lt_items.append(n.data)
         else:
             gt_eq_items.append(n.data)
