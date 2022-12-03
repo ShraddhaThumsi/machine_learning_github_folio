682f8f8f69eda726840919c4de5d40c502435957
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-12-03 14:51:14 +0530

    started set of stacks, need to finish it and start trees

---
+++ fundamentals/algorithms/stacks_and_queues/set_of_stacks.py
@@ -0,0 +1,9 @@
+import fundamentals.data_structures.stack_using_ll as stack
+import fundamentals.data_structures.linked_list as linked_list
+class SetOfStacks:
+    def __init__(self,stacks=[], max_size=10):
+        self.stack_set = stacks
+        self.maxsize_per_stack = max_size
+    def push(self,item):
+        stacknode = linked_list.ListNode(item)
+        least_occupied_stack = self.stack_set.pop()
