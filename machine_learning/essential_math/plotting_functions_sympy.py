192bf02bfd6cec14b8a457e285c6bb613611b814
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2022-12-28 07:50:59 +0530

    continuing basic plotting on sympy

--- machine_learning/essential_math/plotting_functions_sympy.py
+++ machine_learning/essential_math/plotting_functions_sympy.py
@@ -1,5 +1,5 @@
 from sympy import *
-
+from sympy.plotting import plot3d
 
 def linear_function():
     x = symbols('x')
@@ -17,3 +17,19 @@ def quadratic_function():
 
 
 quadratic_function()
+
+
+def plot_plane():
+    x, y = symbols('x y')
+    f_x_y = 2*x + 3*y
+    plot3d(f_x_y)
+
+
+plot_plane()
+
+
+def plot_summation(list_of_items, multiplier):
+    summation = sum(multiplier*list_of_items[i] for i in range(0, len(list_of_items)))
+    plot(summation)
+
+plot_summation([1,2,5,2,3],3)
